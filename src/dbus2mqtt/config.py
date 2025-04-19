e80f3f38705a7486c31c14c3988a0148c7cf5675
Jan-Willem Mulder <jwnmulder@gmail.com>
2025-04-19 15:09:57 +0200

    removed pattern matching for paths as its not tested at all

--- src/dbus2mqtt/config.py
+++ src/dbus2mqtt/config.py
@@ -125,7 +125,7 @@ class DbusConfig:
     def get_subscription_configs(self, bus_name: str, path: str) -> list[SubscriptionConfig]:
         res: list[SubscriptionConfig] = []
         for subscription in self.subscriptions:
-            if fnmatch.fnmatchcase(bus_name, subscription.bus_name) and fnmatch.fnmatchcase(path, subscription.path):
+            if fnmatch.fnmatchcase(bus_name, subscription.bus_name) and path == subscription.path:
                 res.append(subscription)
         return res
 
